80d3fb31ae26b60a51aff60cb3e452249d4c2212
piero965d <piero965d@gmail.com>
2022-11-21 02:44:29 -0500

    valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <h1>Control de Gastos</h1>
-  )
+    const [presupuesto, setPresupuesto] = useState(0)
+    const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+    return (
+
+        <div>
+          <Header
+            presupuesto = {presupuesto}
+            setPresupuesto = {setPresupuesto}
+            isValidPresupuesto = {isValidPresupuesto}
+            setIsValidPresupuesto = {setIsValidPresupuesto}
+          />
+        </div>
+    )
 }
 
 export default App
